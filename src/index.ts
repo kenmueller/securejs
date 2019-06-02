a98c57bb66182b0ce863c085e724e0c0c77714e0
Ken Mueller <kenmueller0@gmail.com>
2019-06-01 17:00:14 -0700

    options

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
-import { ID } from '../types'
+import { ID, SecureOptions } from '../types'
 
 export function makeId(length: number, each: (index: number) => any): string {
 	return range(length).map(each).join('')
 }
 
-export function newId(length: number, type?: ID): string {
-	const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+export function newId(length: number, type?: ID, options?: SecureOptions): string {
+	const letters = lettersForCase((options || {}).case)
 	switch (type) {
 	case undefined:
 	case 'any':
-		return makeId(length, () => characters.charAt(random(62)))
+		const all = letters.concat('0123456789')
+		return makeId(length, () => all.charAt(random(all.length)))
 	case 'number':
 		return makeId(length, () => random(10))
 	case 'string':
-		return makeId(length, () => characters.charAt(random(52)))
+		return makeId(length, () => letters.charAt(random(letters.length)))
 	default:
 		return makeId(length, () => 0)
+	}
+}
+
+function lettersForCase(letterCase: string | undefined): string {
+	const lowercase = 'abcdefghijklmnopqrstuvwxyz'
+	const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	switch (letterCase) {
+	case 'uppercase':
+		return uppercase
+	case 'lowercase':
+		return lowercase
+	default:
+		return lowercase.concat(uppercase)
 	}
 }
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,18 @@
 export type ID = 'number' | 'string' | 'any'
 
+export interface SecureOptions {
+	/**
+	 * Letter capitalization
+	 * 
+	 * `uppercase`: Make all the letters upppercase
+	 * 
+	 * `lowercase`: Make all the letters lowercase
+	 * 
+	 * `any` (default): Randomize the capitalization
+	 */
+	case?: 'uppercase' | 'lowercase' | 'any'
+}
+
 /**
  * Creates a new ID with the specified character generation function
  * @param length The length of the new ID
@@ -8,8 +21,9 @@ export type ID = 'number' | 'string' | 'any'
 export function makeId(length: number, each: (index: number) => any): string
 
 /**
- * Creates a new ID with the specified length and type
+ * Creates a new ID with the specified length, type, and options
  * @param length The length of the new ID
  * @param type Default is "any". Can be "any": numbers and letters, "number": just numbers, "string": just letters
+ * @param options To further customize the ID creation. Must explicitly specify the `type` if you want to add options
  */
-export function newId(length: number, type?: ID): string
+export function newId(length: number, type?: ID, options?: SecureOptions): string
